37a4c890691a39717741574748a164544c9dc47d
camilagv1 <mcgrajalesv01@gmail.com>
2023-06-08 22:43:05 -0600

    api nueva con bd corregida

--- app/controllers/ambientes.controller.js
+++ app/controllers/ambientes.controller.js
@@ -30,13 +30,11 @@ export const findAllAmbientes = async (req, res) => {
     const cantidad_mesas = req.body.cantidad_mesas;
     const num_aprendices = req.body.num_aprendices;
     const num_equipos = req.body.num_equipos;
-    const id_herramientas = req.body.id_herramientas;
-    const id_horario = req.body.id_horario;
-    const id_material = req.body.id_material;
+
 
  
     try {
-       const result = await pool.query(`CALL spInsertAmbientes('${cantidad_sillas}','${cantidad_mesas}','${num_aprendices}','${num_equipos}','${id_herramientas}','${id_horario}','${id_material}');`);
+       const result = await pool.query(`CALL spInsertAmbientes('${cantidad_sillas}','${cantidad_mesas}','${num_aprendices}','${num_equipos}');`);
        res.json(result);
     } catch (error) {
        console.error("Ha ocurrido un error" + error);
@@ -64,13 +62,11 @@ export const findAllAmbientes = async (req, res) => {
     const cantidad_mesas = req.body.cantidad_mesas;
     const num_aprendices = req.body.num_aprendices;
     const num_equipos = req.body.num_equipos;
-    const id_herramienta = req.body.id_herramienta;
-    const id_horario = req.body.id_horario;
-    const id_material = req.body.id_material;
+
 
  
     try {
-       const result = await pool.query(`CALL spUpdateAmbientes(${id},'${cantidad_sillas}','${cantidad_mesas}','${num_aprendices}','${num_equipos}','${id_herramienta}','${id_horario}','${id_material}');`)
+       const result = await pool.query(`CALL spUpdateAmbientes(${id},'${cantidad_sillas}','${cantidad_mesas}','${num_aprendices}','${num_equipos}');`)
        if (result[0].affectedRows != 0)
           res.json(result);
        else
--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _app = _interopRequireDefault(require("./app.js"));
-var _index = require("./config/index.js");
-//usar la funcion del express
-_app["default"].listen(_app["default"].get("PORT"), _index.caseEntorno);
