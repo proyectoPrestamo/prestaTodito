8f52bbcf02bcd99f46316db2122b7c54c3f7937d
Juan3728825 <jmmartinez4817@misena.edu.co>
2023-06-15 15:26:24 -0500

    ARREGLO USER

--- app/config/database/proyectoprestamo (1).sql
+++ app/config/database/proyectoprestamo (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-06-2023 a las 22:17:39
+-- Tiempo de generación: 15-06-2023 a las 22:25:22
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
